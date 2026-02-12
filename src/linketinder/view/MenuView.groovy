46e65bdf47528e08c09d3ab929bdb0be1988abd5
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-02-12 17:43:05 -0300

    feature/menu [fim]: [feat] Menu view

--- src/linketinder/view/MenuView.groovy
+++ src/linketinder/view/MenuView.groovy
@@ -1,4 +1,18 @@
 package linketinder.view
 
+import java.util.Scanner
+
 class MenuView {
+
+    private static final Scanner scanner = new Scanner(System.in)
+
+    static int mostrarMenu() {
+        println "\n=== LINKETINDER ==="
+        println "1 - Listar candidatos"
+        println "2 - Listar empresas"
+        println "0 - Sair"
+        print "Escolha:"
+
+        return scanner.nextInt()
+    }
 }
