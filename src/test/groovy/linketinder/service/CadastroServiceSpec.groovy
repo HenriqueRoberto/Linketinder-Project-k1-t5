6e428446ef4d1646c4568cd5c23cc078c4d3bacc
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-02-17 19:51:29 -0300

    feature/spok-cadastro-tests[fim]: Adicionando classe de teste de cadastro

--- src/test/groovy/linketinder/service/CadastroServiceSpec.groovy
+++ src/test/groovy/linketinder/service/CadastroServiceSpec.groovy
@@ -3,4 +3,12 @@ package linketinder.service
 import spock.lang.Specification
 
 class CadastroServiceSpec extends Specification{
+    def "deve inserir candidato na lista"() {
+        given:
+        //preparar
+        when:
+        //ação
+        then:
+        //verificação
+    }
 }
