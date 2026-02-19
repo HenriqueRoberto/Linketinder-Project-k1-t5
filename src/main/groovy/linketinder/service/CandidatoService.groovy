6061a2d64d61fcc195ca123770abf6ee6d931e20
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-02-19 17:32:10 -0300

    feature/cadastro[feat]: Criação das classes CandidatoService e EmpresaService para verificar os testes

--- src/main/groovy/linketinder/service/CandidatoService.groovy
+++ src/main/groovy/linketinder/service/CandidatoService.groovy
@@ -1,4 +1,17 @@
 package linketinder.service
 
+import linketinder.model.Candidato
+import linketinder.data.DadosMock
+
 class CandidatoService {
+    // Lista iniciada com os mocks
+    private static List<Candidato> candidatos = new ArrayList<>(DadosMock.candidatos())
+
+    static void cadastrar(Candidato candidato) {
+        candidatos.add(candidato)
+    }
+
+    static List<Candidato> listar() {
+        return candidatos
+    }
 }
--- src/main/groovy/linketinder/service/EmpresaService.groovy
+++ src/main/groovy/linketinder/service/EmpresaService.groovy
@@ -1,4 +1,16 @@
 package linketinder.service
 
+import linketinder.model.Empresa
+import linketinder.data.DadosMock
+
 class EmpresaService {
+    private static List<Empresa> empresas = new ArrayList<>(DadosMock.empresas())
+
+    static void cadastrar(Empresa empresa) {
+        empresas.add(empresa)
+    }
+
+    static List<Empresa> listar() {
+        return empresas
+    }
 }
