b9de519f81ff06aa5af7779060abba412ab6d645
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-02-19 17:33:51 -0300

    feature/cadastro[feat]: (model) Adiciona campo senha e construtores para suportar cadastro e login e reestrutura Candidato e Empresa com uso de super

--- src/main/groovy/linketinder/model/Candidato.groovy
+++ src/main/groovy/linketinder/model/Candidato.groovy
@@ -1,4 +1,7 @@
 package linketinder.model
 
 class Candidato extends PessoaFisica{
+    Candidato(String nome, String email, String cpf, int idade, String estado, String cep, String descricao, List<String> competencias, String senha){
+        super(nome, email, cpf, idade, estado, cep, descricao, competencias, senha)
+    }
 }
--- src/main/groovy/linketinder/model/PessoaFisica.groovy
+++ src/main/groovy/linketinder/model/PessoaFisica.groovy
@@ -2,6 +2,7 @@ package linketinder.model
 
 class PessoaFisica implements Pessoa {
 
+    // Atributos privados
     String nome
     String email
     String cpf
@@ -9,17 +10,34 @@ class PessoaFisica implements Pessoa {
     String estado
     String cep
     String descricao
-    List<String> competencias = []
+    List<String> competencias
+    String senha
 
-    String getNome() { nome }
-    String getEmail() { email }
-    String getEstado() { estado }
-    String getCep() { cep }
-    String getDescricao() { descricao }
-    List<String> getCompetencias() { competencias }
+    // Construtor (Necessário para o TDD e para os Services)
+    PessoaFisica(String nome, String email, String cpf, int idade, String estado, String cep, String descricao, List<String> competencias, String senha) {
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.idade = idade
+        this.estado = estado
+        this.cep = cep
+        this.descricao = descricao
+        this.competencias = competencias
+        this.senha = senha
+    }
 
-    String getCpf() { cpf }
-    int getIdade() { idade }
+    // Implementação explícita da Interface Pessoa
+    @Override String getNome() { return nome }
+    @Override String getEmail() { return email }
+    @Override String getEstado() { return estado }
+    @Override String getCep() { return cep }
+    @Override String getDescricao() { return descricao }
+    @Override List<String> getCompetencias() { return competencias }
+
+    // Getters específicos de Pessoa Física
+    String getCpf() { return cpf }
+    int getIdade() { return idade }
+
 
     @Override
     String toString() {
